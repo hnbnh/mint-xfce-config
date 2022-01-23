7e6761cade58758baae2e4e654cbcae42d5c5d3f
hnxb <30380682+hnbnh@users.noreply.github.com>
2022-01-23 16:24:55 +0700

    chore: use apt instead of apt-get

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -6,20 +6,19 @@ apps() {
 	sudo add-apt-repository multiverse
 
 	# zsh, pure prompt
-	sudo apt-get install zsh -y
+	sudo apt install zsh -y
 	chsh -s $(which zsh)
 	mkdir -p "$HOME/.zsh"
 	git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
 	echo "fpath+=$HOME/.zsh/pure" >~/.zshrc
 
 	# docker
-	sudo apt-get install ca-certificates curl gnupg lsb-release -y
+	sudo apt install ca-certificates curl gnupg lsb-release -y
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 	echo \
 		"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-	sudo apt-get update
-	sudo apt-get install docker-ce docker-ce-cli containerd.io
+	sudo apt update && sudo apt install docker-ce docker-ce-cli containerd.io
 	sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 	sudo chmod +x /usr/local/bin/docker-compose
 	sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
--- scripts/config.sh
+++ scripts/config.sh
@@ -5,8 +5,6 @@ config() {
 	# https://askubuntu.com/a/719551
 	curl -s http://mirrors.ubuntu.com/mirrors.txt | xargs -n1 -I {} sh -c 'echo `curl -r 0-102400 -s -w %{speed_download} -o /dev/null {}/ls-lR.gz` {}' | sort -g -r | head -1 | awk '{ print $2  }'
 
-	sudo apt update && sudo apt upgrade -y
-
 	# disable window grouping
 	xfconf-query --channel 'xfce4-panel' --property '/plugins/plugin-3/grouping' --set 0
 
@@ -21,7 +19,7 @@ config() {
 
 	# disable middle mouse button click to paste
 	# https://askubuntu.com/a/1144039
-	sudo apt install xbindkeys xsel xdotool
+	sudo apt update && sudo apt install xbindkeys xsel xdotool
 	echo "\"echo -n | xsel -n -i; pkill xbindkeys; xdotool click 2; xbindkeys\"\nb:2" >~/.xbindkeysrc
 	xbindkeys -p
 	echo "[Desktop Entry]\nType=Application\nName=Disable middle click\nComment=Disable middle mouse button click to paste\nExec=xbindkeys -p\nOnlyShowIn=XFCE\nRunHook=0\nHidden=false" >~/.config/autostart/disable-middle-click.desktop
