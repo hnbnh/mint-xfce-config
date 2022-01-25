6b5bf45b978841d6e6ddb085fc3f98c03ee8ec96
hnxb <30380682+hnbnh@users.noreply.github.com>
2022-01-25 09:17:14 +0700

    refactor(apps): remove winecfg

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -54,7 +54,6 @@ apps() {
 	sudo apt-key add winehq.key
 	sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
 	sudo apt update && sudo apt install --install-recommends winehq-stable -y
-	winecfg
 
 	# =====================================
 	cwd=$(pwd)
