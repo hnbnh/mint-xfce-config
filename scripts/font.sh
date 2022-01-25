fd311b2596476022ec2093451c135d4af8b60de6
hnxb <30380682+hnbnh@users.noreply.github.com>
2022-01-25 09:06:50 +0700

    feat(font): silently accept the license

--- scripts/font.sh
+++ scripts/font.sh
@@ -4,6 +4,8 @@ font() {
 	# ttf-ms-fonts
 	sudo add-apt-repository multiverse
 	sudo apt update
+	# https://askubuntu.com/a/25614
+	echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
 	sudo apt install ttf-mscorefonts-installer -y
 	sudo fc-cache -f -v
 }
