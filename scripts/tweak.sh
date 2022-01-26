5964dbb1ec7fdbf8bce2bf31d4cd3e75008c9a54
hnbnh <huynhninhbinh123@gmail.com>
2022-01-26 23:32:42 +0700

    feat(tweak): add command to update installed theme & icon

--- scripts/tweak.sh
+++ scripts/tweak.sh
@@ -4,11 +4,14 @@ tweak() {
 	# icon
 	sudo add-apt-repository ppa:papirus/papirus -y
 	sudo apt update && sudo apt install papirus-icon-theme
+	xfconf-query -c xsettings -p /Net/IconThemeName -s Papirus
 
 	# theme
 	git clone https://github.com/vinceliuice/Orchis-theme.git ~/Downloads/Orchis-theme
 	sudo -u $USERNAME bash ~/Downloads/Orchis-theme/install.sh -t green
 	rm -rf ~/Downloads/Orchis-theme
+	xfconf-query -c xsettings -p /Net/ThemeName -s Orchis-green-dark-compact
+	xfconf-query -c xfwm4 -p /general/theme -s Orchis-green-dark-compact
 
 	# TODO: disable fx
 }
