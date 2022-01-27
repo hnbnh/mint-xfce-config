8a76969b5081cb379048cafb42996ffb7587e17d
hnbnh <huynhninhbinh123@gmail.com>
2022-01-27 16:05:53 +0700

    feat(apps): add TODO

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -32,7 +32,7 @@ add() {
 	echo "inoremap jk <esc>\ncnoremap jk <C-C>" >>~/.config/nvim/init.vim
 
 	# ungoogled-chromium
-	# TODO: update codename
+	# TODO: use unportable version
 	echo 'deb http://download.opensuse.org/repositories/home:/ungoogled_chromium/Ubuntu_Focal/ /' | sudo tee /etc/apt/sources.list.d/home-ungoogled_chromium.list >/dev/null
 	curl -s 'https://download.opensuse.org/repositories/home:/ungoogled_chromium/Ubuntu_Focal/Release.key' | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home-ungoogled_chromium.gpg >/dev/null
 	sudo apt update && sudo apt install ungoogled-chromium -y
