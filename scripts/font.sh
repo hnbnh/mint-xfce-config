d183b25332d2f499e36bbaef2a30ba44475a73bb
hnbnh <30380682+hnbnh@users.noreply.github.com>
2022-01-27 19:39:20 +0700

    feat(font): add fira code

--- scripts/font.sh
+++ scripts/font.sh
@@ -7,6 +7,10 @@ font() {
 	# https://askubuntu.com/a/25614
 	echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
 	sudo apt install ttf-mscorefonts-installer -y
+
+	# fira code
+	sudo apt install fonts-firacode
+
 	sudo fc-cache -f -v
 }
 
